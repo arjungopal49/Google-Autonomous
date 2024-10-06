211ec5223acc91a2485e36924676d55f6d04b48f
Anmol <agupta358@wisc.edu>
2024-10-06 15:45:38 -0500

    Initialize React app setup for frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import logo from './g-a.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          The inital frontend page for google-autonomous
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
